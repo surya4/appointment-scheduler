2c6b8eaf5f80e7ade5014b74518d8743415a5b04
Surya Singh <me@suryasingh.xyz>
2017-10-10 01:15:34 +0530

    json edit done

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -1,13 +1,32 @@
-let users = require('../../config/users.json');
+const path = require('path');
+let fs = require('fs');
+let jsonFile = path.join(__dirname, "../../config/users.json");
+let users = require("../../config/users.json");
+
 
 exports.appoint_post = (req, res, next) => {
-    for (var i = 0; i < users.length; i++) {
-        var element = array[i];
-
+    let inp = req.body,
+        pos;
+    for (var i = 0; i < users.data.length; i++) {
+        if (users.data[i].name === req.body.scheduleto) {
+            pos = i;
+            break;
+        }
     }
+    let jsonData;
+    fs.readFile(jsonFile, 'utf8', function(err, data) {
+        if (err) throw err;
+        jsonData = JSON.parse(data);
+        console.log(jsonData);
+        jsonData.data[pos].schedules.push(req.body.calendar);
+        fs.writeFile(jsonFile, JSON.stringify(jsonData), function(err) {
+            if (err) throw err;
+            console.log('complete');
+        });
+    });
 
     try {
-        console.log(time.calendar);
+        //   console.log(users.data[pos].password);
     } catch (e) {
         console.log("Error : " + e);
         return next(e)
